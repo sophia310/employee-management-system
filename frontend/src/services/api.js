8108660b1af87971af96394a57b71b4b6222c206
sophia310 <sophiapattekari02@gmail.com>
2026-06-25 18:02:37 +0530

    Configured frontend API for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-API.interceptors.request.use((req) => {
+API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
   if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`;
   }
 
-  return req;
+  return config;
 });
 
 export default API;
